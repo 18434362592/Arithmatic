b24a75a98a4f5e98f2458ee4bf0391f097dbbba7
18434362592 <18434362592@outlook.com>
2017-03-03 07:26:36 -0800

    finished

--- Introduction/src/firework.c
+++ Introduction/src/firework.c
@@ -3,7 +3,7 @@
 #define P 70
 static double k1,k2,k3,k4,k5,k6;
 static int times=0;
-void compute_it(double nC,double nH,double nO,double nN,double Kw);
+int compute_it(double nC,double nH,double nO,double nN,double Kw,double *nCO2_cp,double *nCO_cp,double *nH2O_cp,double *nH2_cp,double *nN2_cp);
 void second(double *nCO2_1,double *nCO_1,double *nH2O_1,double *nH2_1,double *nN2_1,double nC_0,double nH_0,double nO_0,double nN_0,double Kw);
 
 void fires(double nC_0,double nH_0,double nO_0,double nN_0,double Kw)
@@ -14,9 +14,11 @@ void fires(double nC_0,double nH_0,double nO_0,double nN_0,double Kw)
 	double nH2O_1 =nO_0 -nC_0 -nCO2_1;
 	double nH2_1 = nH_0/2 -nH2O_1;
 	double nN2_1=nN_0 /2;
-	printf("A is %.4lf\nnCO2_1 is %.4lf\nnCO_1 is %.4lf\nnH2O_1 is %.4lf\nnH2_1 is %.4lf\nnN2_1 is %.4lf\n ",A,nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1);
+	printf("this is %d time\n",times++);
+	printf("nCO2 is %.4lf\nnCO  is %.4lf\nnH2O is %.4lf\nnH2  is %.4lf\nnN2  is %.4lf\n",nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1);
+	printf("nH is %.4lf\nnOH is %.4lf\nnNO is %.4lf\nnO is %.4lf\nnO2 is %.4lf\n\n\n\n",0.0,0.0,0.0,0.0,0.0);
 	//check
-	double temp1 =nCO2_1 + nCO_1;
+	/*double temp1 =nCO2_1 + nCO_1;
 	double temp2 =2*nH2O_1 + 2*nH2_1;
 	double temp3 =2*nCO2_1 + nCO_1 + nH2O_1;
 	double temp4 =2*nN2_1;
@@ -27,7 +29,7 @@ void fires(double nC_0,double nH_0,double nO_0,double nN_0,double Kw)
 	//check Kw
 	printf("checking Kw************\n");
 	double temp5 = (nCO_1 * nH2O_1)/(nCO2_1*nH2_1);
-	printf("Kw is %.4lf\n\n\n\n",Kw);
+	printf("Kw is %.4lf\n\n\n\n",Kw);*/
 	
 	//call up function second
 	k1=0.3293;
@@ -52,15 +54,23 @@ void second(double *nCO2_1,double *nCO_1,double *nH2O_1,double *nH2_1,double *nN
 	double nNO_2 = k4*(*nH2O_1)*(sqrt(*nN2_1))/((*nH2_1)*sqrt(P/(n_1)));
 	double nO_2 = k6*(*nH2O_1)/((*nH2_1)*P/(n_1));
 	double nO2_2 = (k1*(*nCO2_1))*(k1*(*nCO2_1))/((*nCO_1)*(*nCO_1)*P/(n_1));
-	
+	printf("this is %d time\n",times);
+	times++;
+	printf("nH is %.4lf\nnOH is %.4lf\nnNO is %.4lf\nnO is %.4lf\nnO2 is %.4lf\n",nH2_2,nOH_2,nNO_2,nO_2,nO2_2);
 	double n0C_2=nC_0;
 	double n0H_2 = nH_0 -nH2_2 - nOH_2;
 	double n0O_2 = nO_0 - nOH_2 - nNO_2;
 	double n0N_2 = nN_0 - nNO_2;
-	compute_it(n0C_2,n0H_2,n0O_2,n0N_2,Kw);
+	int continue1=compute_it(n0C_2,n0H_2,n0O_2,n0N_2,Kw,nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1);
+	
+	//continue???
+	if(continue1 ==1)
+	{
+		second(nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1,nC_0,nH_0,nO_0,nN_0,Kw);
+	}
 }
 
-void compute_it(double nC,double nH,double nO,double nN,double Kw)
+int compute_it(double nC,double nH,double nO,double nN,double Kw,double *nCO2_cp,double *nCO_cp,double *nH2O_cp,double *nH2_cp,double *nN2_cp)
 {
 	double A=((nC+(nH / 2) -nO)*Kw + nO) / (2*(Kw-1));
 	double nCO2_1 = -A + sqrt(A*A + nC*(nO - nC)/(Kw-1));
@@ -68,9 +78,9 @@ void compute_it(double nC,double nH,double nO,double nN,double Kw)
 	double nH2O_1 =nO -nC -nCO2_1;
 	double nH2_1 = nH/2 -nH2O_1;
 	double nN2_1=nN /2;
-	printf("A is %.4lf\nNCO2_1 is %.4lf\nnCO_1 is %.4lf\nnH2O_1 is %.4lf\nnH2_1 is %.4lf\nnN2_1 is %.4lf\n ",A,nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1);
+	printf("nCO2 is %.4lf\nnCO is %.4lf\nnH2O is %.4lf\nnH2 is %.4lf\nnN2 is %.4lf\n\n\n\n",nCO2_1,nCO_1,nH2O_1,nH2_1,nN2_1);
 	//checking*********
-	double temp1 =nCO2_1 + nCO_1;
+	/*double temp1 =nCO2_1 + nCO_1;
 	double temp2 =2*nH2O_1 + 2*nH2_1;
 	double temp3 =2*nCO2_1 + nCO_1 + nH2O_1;
 	double temp4 =2*nN2_1;
@@ -81,8 +91,31 @@ void compute_it(double nC,double nH,double nO,double nN,double Kw)
 	//check Kw
 	printf("checking Kw************\n");
 	double temp5 = (nCO_1 * nH2O_1)/(nCO2_1*nH2_1);
-	printf("Kw is %.4lf\n",Kw);
+	printf("Kw is %.4lf\n",Kw);*/
+	
+	//continue??
+	if((fabs(*nCO2_cp - nCO2_1) > 0.0001) || (fabs(*nCO_cp - nCO_1) > 0.0001) || (fabs(*nH2O_cp - nH2O_1) > 0.0001) ||(fabs(*nH2_cp - nH2_1) > 0.0001)||(fabs(*nN2_cp - nN2_1) > 0.0001))
+	{
+		*nCO2_cp=nCO2_1;
+		*nCO_cp=nCO_1;
+		*nH2O_cp = nH2O_1;
+		*nH2_cp = nH2_1;
+		*nN2_cp = nN2_1;
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
 }
+
+
+
+
+
+
+
+
 
 
 
